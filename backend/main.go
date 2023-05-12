e8603e23dd4cad7272c608af442a8a79c6903911
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-05-12 23:03:00 +0900

    テスト用の動くだけの簡易webサーバー

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	err := http.ListenAndServe(
+		":1800",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello,%s!", r.URL.Path[1:])
+		}),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
