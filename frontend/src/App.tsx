d54070b6bf1d041441e3e554870084a78c318cf0
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-06-06 11:52:30 +0900

    admin関係のルーターを追加#6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NewAccountCreate from "./pages/NewAccountCreate";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import AdminLogin from "./pages/AdminLogin";
+import AdminHome from "./pages/AdminHome";
+import AddClubMatch from "./pages/AddClubMatch";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/newaccountcreate" element={<NewAccountCreate />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/adminlogin" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminHome />} />
+        <Route path="/admin/addclubmatch" element={<AddClubMatch />} />
       </Routes>
     </BrowserRouter>
   );
