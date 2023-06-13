b7b66e85ff082cc975f598f4933d0a511b853121
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-06-13 10:09:28 +0900

    ロジック部分とUI部分のコードを分離#6

--- frontend/src/pages/AddClubMatch.tsx
+++ frontend/src/pages/AddClubMatch.tsx
@@ -1,7 +1,113 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 import AddClubMatchTemplate from "../templates/AddClubMatchTemplate";
 
 const AddClubMatch = () => {
-  return <AddClubMatchTemplate />;
+  const navigate = useNavigate();
+  const accessToken = localStorage.getItem("accessToken");
+  const [date, setDate] = useState<Date | null>(null);
+  const [dateEmpty, setDateEmpty] = useState(true);
+  const [voteDate, setVoteDate] = useState<Date | null>(null);
+  const [voteDateEmpty, setVoteDateEmpty] = useState(true);
+  const [title, setTitle] = useState("");
+  const [titleEmpty, setTitleEmpty] = useState(false);
+  const [inputError, setInputError] = useState(false);
+  const [allEmptyError, setAllEmptyError] = useState(true);
+
+  const handleDateChange = (dateValue: Date | null) => {
+    setDate(dateValue);
+    handleDateEmptyChange(dateValue);
+    handleAllEmptyError();
+  };
+  const handleDateEmptyChange = (dateValue: Date | null) => {
+    if (dateValue === null) {
+      setDateEmpty(true);
+    } else {
+      setDateEmpty(false);
+    }
+  };
+  const handleVoteDateChange = (dateValue: Date | null) => {
+    setVoteDate(dateValue);
+    handleVoteDateEmptyChange(dateValue);
+    handleAllEmptyError();
+  };
+  const handleVoteDateEmptyChange = (dateValue: Date | null) => {
+    if (dateValue === null) {
+      setVoteDateEmpty(true);
+    } else {
+      setVoteDateEmpty(false);
+    }
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    handleTitleEmptyChange(event.target.value);
+    handleAllEmptyError();
+  };
+
+  const handleTitleEmptyChange = (titleValue: string) => {
+    if (titleValue === "") {
+      setTitleEmpty(true);
+    } else {
+      setTitleEmpty(false);
+    }
+  };
+
+  const handleAllEmptyError = () => {
+    setAllEmptyError(false);
+  };
+
+  const handleDateSubmit = () => {
+    if (date && voteDate) {
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      const vote_year = voteDate.getFullYear();
+      const vote_month = voteDate.getMonth() + 1;
+      const vote_day = voteDate.getDate();
+      axios
+        .post(
+          "http://localhost:18000/admin",
+          { year, month, day, vote_year, vote_month, vote_day, title },
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          console.log("success login");
+          navigate("/admin");
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log(date);
+          setInputError(true);
+        });
+    }
+  };
+  return (
+    <AddClubMatchTemplate
+      addClubMatchInfo={{
+        date: date,
+        voteDate: voteDate,
+        title: title,
+        dateEmpty: dateEmpty,
+        voteDateEmpty: voteDateEmpty,
+        titleEmpty: titleEmpty,
+        inputError: inputError,
+        allEmptyError: allEmptyError,
+        setInputError: setInputError,
+        handleDateChange: handleDateChange,
+        handleVoteDateChange: handleVoteDateChange,
+        handleTitleChange: handleTitleChange,
+        handleDateSubmit: handleDateSubmit,
+      }}
+    />
+  );
 };
 
 export default AddClubMatch;
--- frontend/src/templates/AddClubMatchTemplate.tsx
+++ frontend/src/templates/AddClubMatchTemplate.tsx
@@ -1,11 +1,86 @@
+import React from "react";
+import { Grid, TextField, Typography } from "@mui/material";
+
 import Header from "../components/Header";
 import Calendar from "../components/Calendar";
+import BaseButton from "../parts/BaseButton";
+import { AddClubMatchInfo } from "../type/velmelazo";
 
-const AddClubMatchTemplate = () => {
+type Props = {
+  addClubMatchInfo: AddClubMatchInfo;
+};
+
+const AddClubMatchTemplate: React.FC<Props> = ({ addClubMatchInfo }) => {
   return (
     <div>
-      <Header headertext={{ text: "Admin Page" }} />
-      <Calendar />
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+      >
+        <Header headertext={{ text: "Admin Page" }} />
+        {addClubMatchInfo.inputError && (
+          <Grid item xs={12} sx={{ pl: "15px", pb: "5px", pt: "30px" }}>
+            <Typography variant="body1" style={{ color: "red" }}>
+              入力に誤りがあります。正しく入力してください。
+            </Typography>
+          </Grid>
+        )}
+        <Grid item sx={{ mt: "50px" }}>
+          <Calendar
+            dateValue={{
+              date: addClubMatchInfo.date,
+              calenderText: "部内戦の日付",
+              handleDateChange: addClubMatchInfo.handleDateChange,
+            }}
+          />
+        </Grid>
+
+        <Calendar
+          dateValue={{
+            date: addClubMatchInfo.voteDate,
+            calenderText: "参加投票の締め切り",
+            handleDateChange: addClubMatchInfo.handleVoteDateChange,
+          }}
+        />
+
+        <Grid item xs={12}>
+          <TextField
+            label="＊タイトル"
+            value={addClubMatchInfo.title}
+            onChange={addClubMatchInfo.handleTitleChange}
+            error={addClubMatchInfo.titleEmpty}
+            helperText={
+              addClubMatchInfo.titleEmpty ? "タイトルを入力してください" : ""
+            }
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <BaseButton
+            baseButton={{
+              buttonText: "日程を追加",
+              onClick: () => {
+                if (
+                  !(
+                    addClubMatchInfo.dateEmpty ||
+                    addClubMatchInfo.titleEmpty ||
+                    addClubMatchInfo.allEmptyError
+                  )
+                ) {
+                  addClubMatchInfo.handleDateSubmit();
+                } else {
+                  addClubMatchInfo.setInputError(true);
+                }
+              },
+              width: "120px",
+              height: "50px",
+              mt: "20px",
+              mb: "",
+            }}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 };
