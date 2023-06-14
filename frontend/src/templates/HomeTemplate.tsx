41ba03f8b06d04cc1275b271dacd971d42320480
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-06-14 17:40:48 +0900

    ユーザーのホームに部内戦情報を表示するように修正#11

--- frontend/src/templates/HomeTemplate.tsx
+++ frontend/src/templates/HomeTemplate.tsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import ClubMatchList from "../components/ClubMatchList";
 
 const HomeTemplate = () => {
   return (
     <div>
       <Header headertext={{ text: "My Page" }} />
+      <ClubMatchList
+        getUrl={{ url: "http://localhost:18000/home", isAdmin: false }}
+      />
     </div>
   );
 };
