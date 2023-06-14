c4c8294046ccb2c2086b22d6cdca1ca67e8a5393
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-06-14 17:36:20 +0900

    参加を扱うデータモデルの作成#11

--- backend/entity/participant.go
+++ backend/entity/participant.go
@@ -7,3 +7,18 @@ type Paticipant struct {
 	ClubMatchID ClubMatchID  `json:"club_match_id" db:"club_match_id"`
 	UserID      UserId       `json:"user_id" db:"user_id"`
 }
+
+type PaticipantClubMatch struct {
+	ID            ClubMatchID `json:"club_match_id" db:"club_match_id"`
+	Year          int         `json:"year" db:"year"`
+	Month         int         `json:"month" db:"month"`
+	Day           int         `json:"day" db:"day"`
+	VoteYear      int         `json:"vote_year" db:"vote_year"`
+	VoteMonth     int         `json:"vote_month" db:"vote_month"`
+	VoteDay       int         `json:"vote_day" db:"vote_day"`
+	Title         string      `json:"title" db:"title"`
+	IsReleased    bool        `json:"is_released" db:"is_released"`
+	IsParticipant bool        `json:"is_participant" db:"is_participant"`
+}
+
+type PaticipantClubMatchs []*PaticipantClubMatch
