c49ed651450851d344b3542b98f77afef9ea06ce
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-06-18 15:27:20 +0900

    チーム確定後の参加者の追加を新規実装#13

---
+++ backend/handler/add_team_member.go
@@ -0,0 +1,70 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/TanakaDaishi0806/Vermelazo.git/backend/entity"
+	"github.com/go-playground/validator/v10"
+)
+
+type AddTeamMember struct {
+	Service   AddTeamMemberService
+	Validator *validator.Validate
+}
+
+func (atm *AddTeamMember) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var p struct {
+		ClubMatchID entity.ClubMatchID `json:"club_match_id" validate:"required"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		RespondJSON(ctx, w, ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+
+	if err := atm.Validator.Struct(&p); err != nil {
+		RespondJSON(ctx, w, ErrResponse{
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+		return
+	}
+
+	cmid := p.ClubMatchID
+
+	lists, err := atm.Service.AddTeamMember(ctx, cmid)
+
+	if err != nil {
+		RespondJSON(ctx, w, ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+
+	rsq := []list{}
+
+	for _, l := range lists {
+		rsq = append(rsq, list{
+			ID:             l.ID,
+			Year:           l.Year,
+			Month:          l.Month,
+			Day:            l.Day,
+			VoteYear:       l.VoteYear,
+			VoteMonth:      l.VoteMonth,
+			VoteDay:        l.VoteDay,
+			Title:          l.Title,
+			IsReleased:     l.IsReleased,
+			IsParticipant:  l.IsParticipant,
+			ParticipantNum: l.ParticipantNum,
+			IsCreateTeam:   l.IsCreateTeam,
+		})
+
+	}
+
+	RespondJSON(ctx, w, rsq, http.StatusOK)
+
+}
