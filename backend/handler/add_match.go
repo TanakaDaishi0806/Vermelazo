b8cb5a478f54d6056f5537d48d3c2c5db6f4931d
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-06-26 18:59:54 +0900

    部内戦の試合の組み合せを作成する機能を実装#16

---
+++ backend/handler/add_match.go
@@ -0,0 +1,65 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/TanakaDaishi0806/Vermelazo.git/backend/entity"
+	"github.com/go-playground/validator/v10"
+)
+
+type AddMatch struct {
+	Service   AddMatchService
+	Validator *validator.Validate
+}
+
+type mlist struct {
+	MatchID     entity.MatchID     `json:"match_id"`
+	ClubMatchID entity.ClubMatchID `json:"club_match_id"`
+	TeamIDA     entity.TeamID      `json:"team_id_a"`
+	TeamIDB     entity.TeamID      `json:"team_id_b"`
+	ScoreA      int                `json:"score_a"`
+	ScoreB      int                `json:"score_b"`
+}
+
+func (am *AddMatch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	var m struct {
+		ClubMatchID entity.ClubMatchID `json:"club_match_id" validate:"required"`
+		MatchNum    int                `json:"match_num" validate:"required"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+		RespondJSON(ctx, w, ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+
+	if err := am.Validator.Struct(m); err != nil {
+		RespondJSON(ctx, w, ErrResponse{
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+		return
+	}
+
+	lists, err := am.Service.AddMatch(ctx, m.ClubMatchID, m.MatchNum)
+	if err != nil {
+		RespondJSON(ctx, w, ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	req := []mlist{}
+
+	for _, l := range lists {
+		req = append(req, mlist{
+			MatchID:     l.MatchID,
+			TeamIDA:     l.TeamIDA,
+			TeamIDB:     l.TeamIDB,
+			ClubMatchID: l.ClubMatchID,
+			ScoreA:      l.ScoreA,
+			ScoreB:      l.ScoreB,
+		})
+	}
+	RespondJSON(ctx, w, req, http.StatusOK)
+}
