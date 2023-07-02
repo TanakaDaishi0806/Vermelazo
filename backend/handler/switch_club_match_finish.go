f5c6d26935796ea06d044470d923d0299f9e9c02
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-07-02 17:29:19 +0900

    部内戦終了時の後処理を実装#16

---
+++ backend/handler/switch_club_match_finish.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/TanakaDaishi0806/Vermelazo.git/backend/entity"
+)
+
+type SwitchClubMatchFinish struct {
+	Repo ClubMatchFinishSwitch
+}
+
+func (scmf *SwitchClubMatchFinish) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	cmid, err := entity.StrTOClubMatchID(r)
+	if err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+
+	lists, err := scmf.Repo.SwitchClubMatchFinish(ctx, cmid)
+
+	if err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	rsq := entity.ClubMatchs{}
+
+	for _, l := range lists {
+		rsq = append(rsq, &entity.ClubMatch{
+			ID:             l.ID,
+			Year:           l.Year,
+			Month:          l.Month,
+			Day:            l.Day,
+			VoteYear:       l.VoteYear,
+			VoteMonth:      l.VoteMonth,
+			VoteDay:        l.VoteDay,
+			Title:          l.Title,
+			IsReleased:     l.IsReleased,
+			ParticipantNum: l.ParticipantNum,
+			IsCreateTeam:   l.IsCreateTeam,
+			IsFinish:       l.IsFinish,
+		})
+	}
+
+	RespondJSON(ctx, w, rsq, http.StatusOK)
+
+}
