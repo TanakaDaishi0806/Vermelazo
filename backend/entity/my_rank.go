ce3ccd568e93146706275305d96b0cbfe2be7132
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-07-13 09:23:18 +0900

    TotalExperienceのjson名を修正

--- backend/entity/my_rank.go
+++ backend/entity/my_rank.go
@@ -6,7 +6,7 @@ type MyRank struct {
 	RankPosition    int  `json:"rank_position" db:"rank_position"`
 	TotalPosition   int  `json:"total_position" db:"toral_position"`
 	RankExperience  int  `json:"rank_experience" db:"rank_experience"`
-	TotalExperience int  `json:"total_expeience" db:"total_experience"`
+	TotalExperience int  `json:"total_experience" db:"total_experience"`
 	GoalNum         int  `json:"goal_num" db:"goal_num"`
 	RankGoal        int  `json:"rank_goal" db:"rank_goal"`
 	TotalGoal       int  `json:"total_goal" db:"total_goal"`
