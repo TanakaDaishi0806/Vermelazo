7324b220face9375fa66772082e145f4944d3fd1
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-07-13 09:41:15 +0900

    試合を追加する際に前にデータがある場合の整合性を取れるように処理を追加

--- backend/store/match.go
+++ backend/store/match.go
@@ -35,6 +35,22 @@ func (mr *MatchRepository) AddMatch(ctx context.Context, mlist entity.Matchs) er
 	sql1 := `update club_match set is_add_match=true where club_match_id=?`
 	sql9 := `select m.match_id,m.club_match_id,tm.user_id from team_member tm left join matchs m on tm.club_match_id=m.club_match_id`
 	sql10 := `insert into match_vote (club_match_id,match_id,user_id) values (?,?,?)`
+	sql11 := `select user_id,count(*) as count from point_getter pg where club_match_id=? group by user_id`
+	sql12 := `update users set goal_num=goal_num-? where user_id=?`
+	sql13 := `update team_rank set point=0,match_num=0,win_num=0,draw_num=0,lose_num=0,goal_num=0,is_last_rank=false where club_match_id=?`
+
+	count := entity.PreGoalNums{}
+	if err := mr.DBQry.SelectContext(ctx, &count, sql11, mlist[0].ClubMatchID); err != nil {
+		return err
+	}
+
+	for _, l := range count {
+		_, err := mr.DBExc.ExecContext(ctx, sql12, l.Count, l.UserID)
+		if err != nil {
+			return err
+		}
+
+	}
 
 	_, err := mr.DBExc.ExecContext(ctx, sql2, mlist[0].ClubMatchID)
 	if err != nil {
@@ -82,6 +98,11 @@ func (mr *MatchRepository) AddMatch(ctx context.Context, mlist entity.Matchs) er
 		return err
 	}
 
+	_, err = mr.DBExc.ExecContext(ctx, sql13, mlist[0].ClubMatchID)
+	if err != nil {
+		return err
+	}
+
 	lists := entity.MatchVotes{}
 	if err := mr.DBQry.SelectContext(ctx, &lists, sql9); err != nil {
 		return err
@@ -120,9 +141,9 @@ func (mr *MatchRepository) AddScore(ctx context.Context, m *entity.Match) error 
 	win_num=win_num-?,draw_num=draw_num-?,lose_num=lose_num-?,goal_num=goal_num-? 
 	where team_id=? and club_match_id=?`
 
-	match := entity.Matchs{}
+	prematch := entity.Matchs{}
 
-	if err := mr.DBQry.SelectContext(ctx, &match, sql2, m.MatchID); err != nil {
+	if err := mr.DBQry.SelectContext(ctx, &prematch, sql2, m.MatchID); err != nil {
 		return err
 	}
 
@@ -137,43 +158,46 @@ func (mr *MatchRepository) AddScore(ctx context.Context, m *entity.Match) error 
 	var draw int = 0
 	var lose int = 0
 
-	//TeamA
-	if match[0].ScoreA > match[0].ScoreB {
-		point = match[0].ScoreA - match[0].ScoreB
-		win = 1
-	} else if match[0].ScoreA == match[0].ScoreB {
+	if prematch[0].ScoreA != -1 {
+
+		//TeamA
+		if prematch[0].ScoreA > prematch[0].ScoreB {
+			point = prematch[0].ScoreA - prematch[0].ScoreB
+			win = 1
+		} else if prematch[0].ScoreA == prematch[0].ScoreB {
+			point = 0
+			draw = 1
+		} else {
+			point = 0
+			lose = 1
+		}
+
+		_, err = mr.DBExc.ExecContext(ctx, sql3, point, win, draw, lose, prematch[0].ScoreA, prematch[0].TeamIDA, prematch[0].ClubMatchID)
+
+		if err != nil {
+			return err
+		}
+
 		point = 0
-		draw = 1
-	} else {
-		point = 0
-		lose = 1
-	}
+		win = 0
+		draw = 0
+		lose = 0
 
-	_, err = mr.DBExc.ExecContext(ctx, sql3, point, win, draw, lose, match[0].ScoreA, match[0].TeamIDA, match[0].ClubMatchID)
+		//TeamB
+		if prematch[0].ScoreA < prematch[0].ScoreB {
+			point = prematch[0].ScoreB - prematch[0].ScoreA
+			win = 1
+		} else if prematch[0].ScoreA == prematch[0].ScoreB {
 
-	if err != nil {
-		return err
-	}
+			draw = 1
+		} else {
+			lose = 1
+		}
+		_, err = mr.DBExc.ExecContext(ctx, sql3, point, win, draw, lose, prematch[0].ScoreB, prematch[0].TeamIDB, prematch[0].ClubMatchID)
 
-	point = 0
-	win = 0
-	draw = 0
-	lose = 0
-
-	//TeamB
-	if match[0].ScoreA < match[0].ScoreB {
-		point = match[0].ScoreB - match[0].ScoreA
-		win = 1
-	} else if match[0].ScoreA == match[0].ScoreB {
-
-		draw = 1
-	} else {
-		lose = 1
-	}
-	_, err = mr.DBExc.ExecContext(ctx, sql3, point, win, draw, lose, match[0].ScoreB, match[0].TeamIDB, match[0].ClubMatchID)
-
-	if err != nil {
-		return err
+		if err != nil {
+			return err
+		}
 	}
 
 	point = 0
