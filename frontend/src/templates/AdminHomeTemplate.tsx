84043a29d857ff02268396bdd153ca6e06d6a9d8
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-07-13 15:42:55 +0900

    管理者画面において、終わった部内戦を表示する機能を実装#16

--- frontend/src/templates/AdminHomeTemplate.tsx
+++ frontend/src/templates/AdminHomeTemplate.tsx
@@ -1,5 +1,6 @@
 import ClubMatchList from "../components/ClubMatchList";
 import Header from "../components/Header";
+import AdminFooter from "../components/AdminFooter";
 
 const AdminHomeTemplate = () => {
   return (
@@ -8,6 +9,7 @@ const AdminHomeTemplate = () => {
       <ClubMatchList
         getUrl={{ url: "http://localhost:18000/admin", isAdmin: true }}
       />
+      <AdminFooter footerValue={{ vnum: 0 }} />
     </div>
   );
 };
