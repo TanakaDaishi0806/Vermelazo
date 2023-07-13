2b72ee27c6f274481cf1fcd24ff4dbce76845b48
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-07-13 09:25:57 +0900

    MatchVoteモデルにTeamIDを追加し,VoteKindNumの型を変更

--- backend/entity/vote.go
+++ backend/entity/vote.go
@@ -1,18 +1,13 @@
 package entity
 
-type VoteKindNum int
 type ID int64
 
+// myteamVote=false,matchVote=true
 type VoteKind struct {
-	VoteKindNum VoteKindNum `json:"vote_kind_num" db:"vote_kind_num"`
+	VoteKindNum bool `json:"vote_kind_num" db:"vote_kind_num"`
 }
 
 type VoteKindNums []*VoteKind
-
-const (
-	MyTeamVoteNum VoteKindNum = 0
-	MatchVoteNum  VoteKindNum = 1
-)
 
 type PositionMom struct {
 	ClubMatchID ClubMatchID `json:"club_match_id" db:"club_match_id"`
@@ -58,6 +53,7 @@ type MatchVote struct {
 	ClubMatchID ClubMatchID `json:"club_match_id" db:"club_match_id"`
 	MatchID     MatchID     `json:"match_id" db:"match_id"`
 	UserID      UserId      `json:"user_id" db:"user_id"`
+	TeamID      TeamID      `json:"team_id" db:"team_id"`
 	IsVote      bool        `json:"is_vote" db:"is_vote"`
 }
 
