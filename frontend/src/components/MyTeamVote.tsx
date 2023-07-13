be1e074c5c1260163c3bf6c303230906bf588ccf
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-07-13 12:00:44 +0900

    自チームへの投票フォームの実装#16

---
+++ frontend/src/components/MyTeamVote.tsx
@@ -0,0 +1,168 @@
+import {
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Typography,
+  ListItemIcon,
+  Checkbox,
+} from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import { TeamMember } from "../type/velmelazo";
+import { MyTeamVoteInfo } from "../type/velmelazo";
+import TextItem from "../parts/TextItem";
+import BaseButton from "../parts/BaseButton";
+
+type Props = {
+  myTeamVoteInfo: MyTeamVoteInfo;
+};
+
+const MyTeamVote: React.FC<Props> = ({ myTeamVoteInfo }) => {
+  const accessToken = localStorage.getItem("accessToken");
+  const navigate = useNavigate();
+  const [teamMember, setTeamMember] = React.useState<TeamMember[]>([]);
+  const [mom, setMom] = React.useState<TeamMember[]>([]);
+  const [submitError, setSubmitError] = React.useState(false);
+
+  React.useEffect(() => {
+    axios
+      .get(myTeamVoteInfo.getUrlMyTeam, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setTeamMember(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleMom = (value: TeamMember) => () => {
+    const currentIndex = mom.indexOf(value);
+    const newMom = [...mom];
+
+    if (currentIndex === -1 && mom.length < 3) {
+      newMom.push(value);
+    } else {
+      newMom.splice(currentIndex, 1);
+    }
+    console.log(mom);
+
+    setMom(newMom);
+  };
+
+  const customList = (items: TeamMember[]) => (
+    <Paper sx={{ width: 160 }}>
+      <List dense component="div" role="list">
+        {items.map((value, index) => {
+          const labelId = `transfer-list-item-${value}-label`;
+
+          return (
+            <ListItem
+              key={index}
+              role="listitem"
+              onClick={handleMom(value)}
+              sx={{ height: "40px", px: "0px", py: "0px" }}
+            >
+              <ListItemIcon>
+                <Checkbox
+                  checked={mom.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{
+                    "aria-labelledby": labelId,
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={value.name} />
+            </ListItem>
+          );
+        })}
+      </List>
+    </Paper>
+  );
+
+  const handlesubmit = () => {
+    if (mom.length >= 1) {
+      mom.map((member) => {
+        axios
+          .post(
+            myTeamVoteInfo.postUrl,
+            {
+              club_match_id: myTeamVoteInfo.club_match_id,
+              match_id: myTeamVoteInfo.match_id,
+              user_id: member.user_id,
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          )
+          .then((response) => {
+            console.log(response.data);
+            navigate(myTeamVoteInfo.toUrl, {
+              state: {
+                club_match_id: myTeamVoteInfo.club_match_id,
+              },
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      });
+    } else {
+      setSubmitError(true);
+    }
+  };
+
+  return (
+    <Grid container justifyContent="center" alignItems="center">
+      <Grid item xs={4}>
+        <Grid container alignItems="center" sx={{ ml: "40px", mt: "40px" }}>
+          <TextItem textItemInfo={{ itemText: "MYチーム MOM" }} />
+          <TextItem textItemInfo={{ itemText: "(3人まで投票可能)" }} />
+        </Grid>
+      </Grid>
+      <Grid item xs={8}></Grid>
+      <Grid item xs={12}>
+        <Grid container justifyContent="center" alignItems="center">
+          {customList(teamMember)}
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container justifyContent="center" alignItems="center">
+          {submitError && (
+            <Typography variant="body1" style={{ color: "red" }}>
+              最低1人は選択してください。
+            </Typography>
+          )}
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Grid container justifyContent="center" alignItems="center">
+          <BaseButton
+            baseButton={{
+              buttonText: "登録",
+              onClick: handlesubmit,
+              width: "150px",
+              height: "50px",
+              mt: "30px",
+              mb: "50px",
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MyTeamVote;
