949f7dcfb15f049eb754ad04be315c77ee90eb2c
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-08-21 14:18:30 +0900

    ユーザーに関する機能追加に伴う関数の追加実装#20

--- backend/store/user.go
+++ backend/store/user.go
@@ -17,6 +17,18 @@ type GetUser struct {
 	DB Queryer
 }
 
+type ChangeUserInfo struct {
+	DB Execer
+}
+
+type ListUserInfo struct {
+	DB Queryer
+}
+
+type ChangeUserPassword struct {
+	DB Execer
+}
+
 func (ru *RegisterUser) RegisterUser(ctx context.Context, requ *entity.User) error {
 	sql := `INSERT INTO users (name,furigana,student_id,password,grade,role,mailaddress,
 		position,experience) VALUES (?,?,?,?,?,?,?,?,?)`
@@ -49,4 +61,43 @@ func (gu *GetUser) GetUser(ctx context.Context, studentID string) (*entity.User,
 		return nil, err
 	}
 	return u, nil
+}
+
+func (cui *ChangeUserInfo) ChangeUserInfo(ctx context.Context, ui *entity.User) error {
+	sql := `update users set name=?,furigana=?,student_id=?,grade=?,mailaddress=?,
+	position=?,experience=? where user_id=?`
+
+	_, err := cui.DB.ExecContext(ctx, sql, ui.Name, ui.Furigana, ui.StudentID, ui.Grade,
+		ui.MailAddress, ui.Position, ui.Experience, ui.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (lui *ListUserInfo) ListUserInfo(ctx context.Context, uid entity.UserId) (*entity.User, error) {
+	sql := `select name,furigana,student_id,grade,mailaddress,position,experience 
+	from users where user_id=?`
+
+	u := &entity.User{}
+
+	if err := lui.DB.GetContext(ctx, u, sql, uid); err != nil {
+		return nil, err
+	}
+	return u, nil
+
+}
+
+func (cup *ChangeUserPassword) ChangeUserPassword(ctx context.Context, uid entity.UserId, p string) error {
+	sql := `update users set password=? where user_id=?`
+
+	_, err := cup.DB.ExecContext(ctx, sql, p, uid)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
