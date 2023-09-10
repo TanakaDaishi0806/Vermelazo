cd17145fea5f1799f73a6b1f758f1c922f0d945c
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-09-10 17:32:45 +0900

    管理者のホームのルーティングを変更

--- frontend/src/pages/AdminLogin.tsx
+++ frontend/src/pages/AdminLogin.tsx
@@ -65,7 +65,7 @@ const AdminLogin = () => {
           })
           .then((response) => {
             console.log(response.data);
-            navigate("/adminhome");
+            navigate("/admin");
           })
           .catch((error) => {
             console.log(error);
