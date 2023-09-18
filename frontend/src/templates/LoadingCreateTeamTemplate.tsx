487e4d0cb110ed9e8d1bfc313b1dd476ec3d475e
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-09-18 15:36:26 +0900

    ローディングの位置を修正

--- frontend/src/templates/LoadingCreateTeamTemplate.tsx
+++ frontend/src/templates/LoadingCreateTeamTemplate.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
-import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 const LoadingCreateTeamTemplate = () => {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Grid
+      container
+      justifyContent="center" // 横方向の中央寄せ
+      alignItems="center" // 縦方向の中央寄せ
+      style={{ height: "100vh" }} // 画面全体の高さいっぱいに設定
+    >
       <CircularProgress />
-    </Box>
+    </Grid>
   );
 };
 
