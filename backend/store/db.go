0fa6735bd4a3253d34e7815fc881a32b4252dd38
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-10-05 09:24:10 +0900

    ホスト名を変更

--- backend/store/db.go
+++ backend/store/db.go
@@ -27,9 +27,9 @@ var (
 func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
-	//host_staging := os.Getenv("127.0.0.1:3309")
+	host := "127.0.0.1:3309"
 	//host := os.Getenv("HOST_STAGING")
-	host := os.Getenv("HOST_PRODUCT")
+	//host := os.Getenv("HOST_PRODUCT")
 	log.Println(host)
 	db, err := sql.Open("mysql",
 		fmt.Sprintf("%s:%s@tcp(%s)/my-database?tls=true", user, password, host),
