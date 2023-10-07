9eed57a9c954ac29adae8bf405cd4a414dd8309e
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-10-07 18:32:30 +0900

    データベース名を環境変数として読み込むように変更

--- backend/store/db.go
+++ backend/store/db.go
@@ -27,8 +27,9 @@ func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("HOST_PRODUCT")
+	db_name := os.Getenv("DB_NAME")
 	db, err := sql.Open("mysql",
-		fmt.Sprintf("%s:%s@tcp(%s)/my-database?tls=true", user, password, host),
+		fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true", user, password, host, db_name),
 		//fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName),
 	)
 	if err != nil {
