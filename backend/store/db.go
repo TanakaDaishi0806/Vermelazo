9819e42b816e7b2614a217fce6a925406fa19496
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-10-07 21:16:32 +0900

    optionを環境変数から読み込みするように変更

--- backend/store/db.go
+++ backend/store/db.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -28,10 +29,13 @@ func New(ctx context.Context) (*sqlx.DB, func(), error) {
 	password := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("HOST_PRODUCT")
 	dbName := os.Getenv("DB_NAME")
+	option := os.Getenv("OPTION")
 	db, err := sql.Open("mysql",
-		fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true", user, password, host, dbName),
+		fmt.Sprintf("%s:%s@tcp(%s)/%s?%s", user, password, host, dbName, option),
+		//fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true", user, password, host, dbName),
 		//fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName),
 	)
+	log.Printf("%s:%s@tcp(%s)/%s?%s", user, password, host, dbName, option)
 	if err != nil {
 		return nil, func() {}, err
 	}
