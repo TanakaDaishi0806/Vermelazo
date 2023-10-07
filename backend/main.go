abf945760ed3d17a74e3ea8096110290b3d5b9a7
TanakaDaishi0806 <dai4dai0806@ezweb.ne.jp>
2023-10-07 20:13:29 +0900

    不要な部分を削除

--- backend/main.go
+++ backend/main.go
@@ -16,18 +16,12 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	// cfg, err := config.New()
-	// if err != nil {
-	// 	return err
-	// }
 	port := os.Getenv("PORT")
 
 	l, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
-	//l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 
 	if err != nil {
 		log.Fatalf("failed to listen port %s: %v", port, err)
-		//log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start wih: %v", url)
